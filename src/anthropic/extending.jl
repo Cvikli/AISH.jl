82e420999478c687e280c726017793932e99bd5b
Cvikli <havlikmarcell@gmail.com>
2024-08-09 16:44:34 +0200

    Improved AI state handling in anthropic_ask_safe function

--- src/anthropic/extending.jl
+++ src/anthropic/extending.jl
@@ -7,7 +7,7 @@ Anthropic.stream_response(messages::Vector{Message}; model::String="claude-3-5-s
 
 Anthropic.ai_stream_safe(ai_state::AIState; model=ai_state.model, max_tokens::Int=1024, printout=true) = Anthropic.ai_stream_safe(to_dict(ai_state); model, max_tokens, printout)
 
-anthropic_ask_safe(ai_state::AIState; model=ai_state.model, return_all=false) = anthropic_ask_safe(cur_conv_msgs(state); model, return_all)
+anthropic_ask_safe(ai_state::AIState; model=ai_state.model, return_all=false) = anthropic_ask_safe(cur_conv_msgs(ai_state); model, return_all)
 anthropic_ask_safe(conversation::Vector{Message}; model, return_all=false) = begin
 	conv = [
 			msg.role == :system ? SystemMessage(msg.content) :
