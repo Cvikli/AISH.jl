4e8d2dfdb6deda0141bfa7dec8cdbf8a16305455
SixZero <havliktomi@hotmail.com>
2024-09-06 13:37:51 +0200

    Some more improvements.

--- src/AI_contexter.jl
+++ src/AI_contexter.jl
@@ -5,12 +5,12 @@ abstract type AbstractContextCreator end
     keep::Int = 7
 end
 
-function get_cache_setting(::AbstractContextCreator)
+function get_cache_setting(::AbstractContextCreator, conv)
     printstyled("WARNING: get_cache_setting not implemented for this contexter type. Defaulting to no caching.\n", color=:red)
     return nothing
 end
 
-function get_cache_setting(::SimpleContexter)
+function get_cache_setting(::SimpleContexter, conv)
     return nothing
 end
 
--- src/process_query.jl
+++ src/process_query.jl
@@ -20,7 +20,7 @@ end
 streaming_process_question(ai_state::AIState, user_question, shell_results=Dict{String, String}()) = begin
     question = prepare_user_message!(ai_state.contexter, ai_state, user_question, shell_results)
     user_msg = add_n_save_user_message!(ai_state, question)
-    cache = get_cache_setting(ai_state.contexter)
+    cache = get_cache_setting(ai_state.contexter, curr_conv(ai_state))
     full_response, user_meta, ai_meta, start_time = ai_stream_safe(ai_state, printout=false, cache=cache)
     full_response, user_meta, ai_meta, start_time, user_msg
 end
@@ -35,14 +35,14 @@ function process_message(state::AIState)
     local ai_meta, msg
 
     if state.streaming
-        cache = get_cache_setting(state.contexter)
+        cache = get_cache_setting(state.contexter, curr_conv(state))
         full_response, user_meta, ai_meta, start_time = ai_stream_safe(state, printout=false, cache=cache) 
         msg = channel_to_string(full_response, cb=() -> (user_meta.elapsed -= start_time; println("\e[34mUser message meta: \e[0m$(format_meta_info(user_meta))")))
         calc_elapsed_times(ai_meta, user_meta.elapsed, start_time)
         update_last_user_message_meta(state, user_meta)
     else
         println("Thinking...")  
-        cache = get_cache_setting(state.contexter)
+        cache = get_cache_setting(state.contexter, curr_conv(state))
         assistant_message = anthropic_ask_safe(state, cache=cache)
         msg = assistant_message.content
         ai_meta = StreamMeta(input_tokens=assistant_message.tokens[1], output_tokens=assistant_message.tokens[2], price=assistant_message.cost, elapsed=assistant_message.elapsed)
