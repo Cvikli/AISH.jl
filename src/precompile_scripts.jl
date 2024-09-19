c6e686539784c73f9da83c7c22bb51db1a5b0f27
Cvikli <havlikmarcell@gmail.com>
2024-09-19 15:34:59 +0200

    all the tests

--- src/precompile_scripts.jl
+++ src/precompile_scripts.jl
@@ -11,10 +11,10 @@ using PrecompileTools
 
 @setup_workload begin
     @compile_workload begin
-        # state = initialize_ai_state()
-        # process_question(state, "Hello, AI assistant!")
-        # process_message(state)
-        # extract_shell_commands("```sh\necho 'Hello, World!'\n```")
-        # start_conversation(state)
+        state = initialize_ai_state()
+        process_question(state, "Hello, AI assistant!")
+        process_message(state)
+        extract_shell_commands("```sh\necho 'Hello, World!'\n```")
+        start_conversation(state, loop=false)
     end
 end
---
+++ test/precompile_aish.jl
@@ -0,0 +1,15 @@
+using AISH
+
+# Simulate a basic run of the main function
+args = ["--project-paths", ".", "--streaming"]
+AISH.parse_commandline()
+state = AISH.initialize_ai_state(no_confirm=true)
+AISH.get_all_conversations_without_messages(state)
+AISH.resume_last_conversation(state)
+AISH.generate_new_conversation(state)
+AISH.update_project_path_and_sysprompt!(state)
+AISH.print_project_tree(state, show_tokens=false)
+
+shell_results = Dict{String, String}()
+# Simulate processing a simple query
+AISH.process_question(state, "say yes", shell_results)
