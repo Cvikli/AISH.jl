3a4d55f385f2569276da0953ecae32e2c93fd5a6
Cvikli <havlikmarcell@gmail.com>
2024-09-30 12:36:17 +0200

    precompile repair

--- test/playground/precompile_aish.jl
+++ test/playground/precompile_aish.jl
@@ -1,18 +1,7 @@
 using AISH
-using AISH: CodeBlock
 using PromptingTools
 using DataStructures
 using BoilerplateCvikli
 
 # Simulate a basic run of the main function
-args = ["--project-paths", ".", "--streaming"]
-AISH.parse_commandline()
-state = AISH.initialize_ai_state(no_confirm=true)
-AISH.get_all_conversations_without_messages(state)
-AISH.resume_last_conversation(state)
-AISH.generate_new_conversation(state)
-AISH.update_project_path_and_sysprompt!(state)
-AISH.print_project_tree(state, show_tokens=false)
-
-# Simulate processing a simple query
-AISH.process_question(state, "say yes")
+AISH.main(loop=false)
