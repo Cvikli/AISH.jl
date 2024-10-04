d4e456d150367b2418a730772cf15d7996d401e9
Cvikli <havlikmarcell@gmail.com>
2024-10-04 14:42:16 +0200

    Enhanced voyage embedder, index builder initialization and retrieval

--- test/test_examples/imposible.jl
+++ test/test_examples/imposible.jl
@@ -5,5 +5,11 @@ using EasyContext: cache_key
 
 using DataStructures
 using EasyContext: CachedLoader
-CachedLoader(loader=JuliaLoader(),memory=Dict{String,OrderedDict{String,String}}())(SourceChunker())
+using EasyContext: create_voyage_embedder, create_combined_index_builder, get_index
+# CachedLoader(loader=JuliaLoader(),memory=Dict{String,OrderedDict{String,String}}())(SourceChunker())
 
+
+@time voyage_embedder = create_voyage_embedder(model="voyage-code-2")
+@time jl_simi_filter = create_combined_index_builder(voyage_embedder; top_k=120)
+@time jl_pkg_index = get_index(jl_simi_filter, CachedLoader(loader=JuliaLoader(),memory=Dict{String,OrderedDict{String,String}}())(SourceChunker()))
+;
