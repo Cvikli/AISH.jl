a653f698732d49858ff7b56d355065414722faeb
SixZero <havliktomi@hotmail.com>
2024-10-07 12:19:34 +0200

    Some more adjustments.

--- src/AISH.jl
+++ src/AISH.jl
@@ -17,11 +17,10 @@ using EasyContext: get_cache_setting
 using EasyContext: codeblock_runner
 using EasyContext: shell_ctx_2_string
 using EasyContext: init_workspace_context
-using EasyContext: init_julia_context
 using EasyContext: init_conversation_context
 using EasyContext: process_workspace_context
 using EasyContext: process_julia_context
-using EasyContext: jl_age!, ws_age!, ageing!
+using EasyContext: cut_old_history!
 using EasyContext: shell_format_description, workspace_format_description, julia_format_description
 using EasyContext
 
@@ -37,6 +36,7 @@ function start_conversation(user_question=""; resume, streaming, project_paths, 
   workspace_context = init_workspace_context(project_paths)
   julia_context     = init_julia_context()
   conv_ctx          = init_conversation_context(SYSTEM_PROMPT(ChatSH), shell_format_description(), workspace_format_description(), julia_format_description())
+  age_tracker       = AgeTracker(max_history=14, cut_to=6)
   
   question_acc      = QuestionCTX()
   extractor         = CodeBlockExtractor()
@@ -44,7 +44,7 @@ function start_conversation(user_question=""; resume, streaming, project_paths, 
 
   # prepare 
   print_project_tree(workspace_context.workspace, show_tokens=show_tokens)
-  set_terminal_title("AISH $(workspace_context.workspace.common_path)")
+  set_terminal_title("AISH $(workspace_context.workspace.root_path)")
   !silent && greet(ChatSH)
   !silent && isempty(user_question) && (isdefined(Base, :active_repl) ? println("Your first [Enter] will just interrupt the REPL line and get into the conversation after that: ") : println("Your multiline input (empty line to finish):"))
 
@@ -56,15 +56,14 @@ function start_conversation(user_question=""; resume, streaming, project_paths, 
 
     ctx_question = user_question |> question_acc 
     ctx_shell    = extractor |> shell_ctx_2_string
-    ctx_codebase = @async_showerr process_workspace_context(workspace_context, ctx_question)
-    ctx_jl_pkg   = @async_showerr process_julia_context(julia_context, ctx_question)
-
+    ctx_codebase = @async_showerr process_workspace_context(workspace_context, ctx_question; age_tracker)
+    ctx_jl_pkg   = @async_showerr process_julia_context(julia_context, ctx_question; age_tracker)
 
     query = context_combiner!(
       user_question, 
       ctx_shell, 
       fetch(ctx_codebase), 
-      fetch(ctx_jl_pkg)
+      fetch(ctx_jl_pkg),
     )
 
     conv_ctx(create_user_message(query))
@@ -72,17 +71,17 @@ function start_conversation(user_question=""; resume, streaming, project_paths, 
     reset!(extractor)
     user_question = ""
 
-    cache = get_cache_setting(workspace_context.ws_age, conv_ctx)
+    cache = get_cache_setting(age_tracker, conv_ctx)
     error = LLM_solve(conv_ctx, cache;
                       on_text     = (text)   -> extract_and_preprocess_codeblocks(text, extractor, preprocess=(cb)->LLM_conditonal_apply_changes(cb)),
                       on_meta_usr = (meta)   -> update_last_user_message_meta(conv_ctx, meta),
                       on_meta_ai  = (ai_msg) -> conv_ctx(ai_msg),
-                      on_done     = ()       -> codeblock_runner(extractor),
+                      on_done     = ()       -> @async_showerr( codeblock_runner(extractor)),
                       on_error    = (error)  -> add_error_message!(conv_ctx,"ERROR: $error"),
     )
-    should_age = ageing!(conv_ctx, workspace_context.ws_age)
-    should_age && jl_age!(julia_context)
-    should_age && ws_age!(workspace_context)
+
+    cut_old_history!(age_tracker, conv_ctx, julia_context, workspace_context, )
+
     silent && break
   end
 end
