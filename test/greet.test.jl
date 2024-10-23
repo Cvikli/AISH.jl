8f8c82ff8ce2728cb3f226faff7d038ca056cf76
Marcell <havlikmarcell@gmail.com>
2024-10-23 23:49:47 +0200

    greet stuffs

--- test/greet.test.jl
+++ test/greet.test.jl
@@ -5,9 +5,9 @@ include("../src/greeting.jl")
 println("Hi from the greet test file!")
 
 @testset "Greeting Test" begin
-    @test greet("User", "Hello") == "Hello, User!"  # Changed order of test cases
-    @test greet("World") == "Hi, World!"  # Changed expectation
-    @test greet("Orion") == "Hi, Orion!"  # Changed expectation
+    @test greet("User", "Hello") == "Hello, User!"
+    @test greet("World") == "Hi, World!"
+    @test greet("Orion") == "Hi, Orion!"
 end
 
 # ... existing code ...
