0fc8ef08fedbecc7edf3092d913ad9e5ff68a032
Cvikli <havlikmarcell@gmail.com>
2024-11-06 14:39:02 +0100

    Graceful exit handling refactor

--- src/utils.jl
+++ src/utils.jl
@@ -1,11 +1,11 @@
-_interrupt_handler = Ref{Function}((sig::Int32) -> nothing)
-handle_interrupt(sig::Int32, id::String) = (println("\nExiting gracefully from conversation $id. Good bye! :)"); exit(0))
-function nice_exit_handler(conv::Union{Conversation,ConversationX})
-    global _interrupt_handler
-    id = conv isa Conversation ? "" : conv.id
-    _interrupt_handler[] = (sig::Int32) -> handle_interrupt(sig, id)
-    ccall(:signal, Ptr{Cvoid}, (Cint, Ptr{Cvoid}), 2, @cfunction(_interrupt_handler[], Cvoid, (Int32,)))
+
+_conversation_id = Ref{String}("")
+
+handle_interrupt(sig::Int32) = (println("\nExiting gracefully from conversation ($(_conversation_id[])). Good bye! :)"); exit(0))
+
+function nice_exit_handler(conv::Union{String,Conversation,ConversationX})
+    global _conversation_id[] = conv isa String ? conv : conv isa Conversation ? "" : conv.id
+    ccall(:signal, Ptr{Cvoid}, (Cint, Ptr{Cvoid}), 2, @cfunction(handle_interrupt, Cvoid, (Int32,)))
 end
 
 set_terminal_title(title::String) = print(IOContext(stdout, :color => true), "\e]0;$title\a")
-
