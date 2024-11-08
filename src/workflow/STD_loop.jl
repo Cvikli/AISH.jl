ba9ea773238602d4adb0162f600ba66871273434
SixZero <havliktomi@hotmail.com>
2024-11-08 14:52:19 +0100

    log_instant_apply fix.

--- src/workflow/STD_loop.jl
+++ src/workflow/STD_loop.jl
@@ -42,13 +42,13 @@ function (model::AIModel)(user_question)
 
     cache = get_cache_setting(model.age_tracker, model.conv_ctx)
     error = LLM_solve(model.conv_ctx, cache;
-                      on_text     = (text)   -> extract_and_preprocess_codeblocks(text, model.extractor, preprocess=(cb)->LLM_conditonal_apply_changes(cb, model.workspace_context.workspace)),
+                      on_text     = (text)   -> extract_and_preprocess_codeblocks(text, model.extractor, preprocess=(cb) -> LLM_conditonal_apply_changes(cb, model.workspace_context.workspace)),
                       on_meta_usr = (meta)   -> update_last_user_message_meta(model.conv_ctx, meta),
                       on_meta_ai  = (ai_msg) -> model.conv_ctx(ai_msg),
                       on_done     = ()       -> @async_showerr(cd(()->codeblock_runner(model.extractor), model.workspace_context)),
                       on_error    = (error)  -> add_error_message!(model.conv_ctx,"ERROR: $error"),
     )
-    log_instant_apply(model.extractor, ctx_question)
+    log_instant_apply(model.extractor, ctx_question, model.workspace_context.workspace)
 
     cut_old_conversation_history!(model.age_tracker, model.conv_ctx, model.julia_context, model.workspace_context)
 
