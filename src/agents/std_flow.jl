525a61e051e48b0571980dfb98632e2c109ed03f
Cvikli <havlikmarcell@gmail.com>
2024-12-19 19:12:52 +0100

    Commented out workspace description in STDFlow

--- src/agents/std_flow.jl
+++ src/agents/std_flow.jl
@@ -32,7 +32,7 @@ mutable struct STDFlow <: Workflow
         )
         m.conv_ctx.system_message.content = SYSTEM_PROMPT(ChatSH; skills, 
         guide_strs=[
-            workspace_format_description(m.workspace_context.workspace), 
+            # workspace_format_description(m.workspace_context.workspace), 
             (use_julia ? julia_format_guide : "")])
         # print_project_tree(m.workspace_context.workspace, summary_callback=LLM_summary)
         # println(workspace_format_description(m.workspace_context.workspace))
