4697e1f3c82c39d41f2dbe1c6c056bd5338b88fd
Cvikli <havlikmarcell@gmail.com>
2025-01-23 13:23:53 +0100

    Renamed 'skills' to 'tools' in start function, adjusted project_paths in test main

--- src/interfaces/cmd/cmd.jl
+++ src/interfaces/cmd/cmd.jl
@@ -12,7 +12,7 @@ function start_conversation(user_question=""; workflow::DataType, resume_data=no
   end
 end
 start(message=""; workflow::DataType, resume_data=nothing, project_paths::String="", kwargs...) = start(message; workflow, resume_data, project_paths=[project_paths], kwargs...)
-start(message=""; workflow::DataType, resume_data=nothing, project_paths::Vector{String}=String[], logdir=LOGDIR, show_tokens=false, no_confirm=false, loop=true, detached_git_dev=true, use_julia=false, skills=DEFAULT_TOOLS) = start_conversation(message; workflow, loop, resume_data, project_paths, logdir, show_tokens, silent=!isempty(message), no_confirm, detached_git_dev, use_julia, skills)
+start(message=""; workflow::DataType, resume_data=nothing, project_paths::Vector{String}=String[], logdir=LOGDIR, show_tokens=false, no_confirm=false, loop=true, detached_git_dev=true, use_julia=false, tools=DEFAULT_TOOLS) = start_conversation(message; workflow, loop, resume_data, project_paths, logdir, show_tokens, silent=!isempty(message), no_confirm, detached_git_dev, use_julia, tools)
 
 function main(;workflow::DataType, tools=DEFAULT_TOOLS, loop=true)
   args = parse_commandline()
